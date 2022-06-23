ea768c2eaa4b3269072123c2a5c8fbcdac6ef91b
Hemant Natani <hemantnatani2002@gmail.com>
2022-06-23 15:40:42 +0530

    second commit

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
+// console.log(date());
 const app = express();
-let newItems = ["buy food", "cook food", "eat food"];
-let workItem = [];
+const newItems = ["buy food", "cook food", "eat food"];
+const workItem = [];
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
-    let today = new Date();
-   let options = {
-    weekday : "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-   }
-   var day = today.toLocaleDateString("en-US", options);
+    let day = date.getDay();
     res.render("list", {listTitle: day, itemlist: newItems});
     
 });
@@ -48,8 +42,8 @@ app.get("/work", function(req,res){
 //     let item = req.body.newitem;
 //     workItem.push(item);
 //     res.redirect("/work");
-// });
+// }); it simply customised to if-else statement in app.get()
 
-app.listen("3000", function(){
+app.listen(process.env.PORt || "3000", function(){
     console.log("server running on 3000");
 });
